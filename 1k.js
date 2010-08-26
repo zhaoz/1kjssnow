75ed4551c5e8cb1bae3ec5ce34f831451f86658f
Ziling Zhao <zilingzhao@gmail.com>
2010-08-25 22:45:06 -0700

    diff sized flakes

--- 1k.js
+++ 1k.js
@@ -1,62 +1,67 @@
-var c = document.getElementById('c'), t = 0, n = 1, f = [], h = 600;
+var c=document.getElementById('c'),t=0,n=2,f=[],h=600,p=Math.PI;
     vf = [
-        [2,3,6,8,2,0],
-        [2,3,6,8,2,0],
-        [2,3,6,8,2,0],
-        [2,3,6,8,2,0],
-        [2,3,6,8,2,0],
-        [2,3,6,8,2,0]
+        [1,1,0,-1,1,-1],
+        [2,1,1,0,-1,-2],
+        [1,2,1,2,-2,-1],
+        [1,2,1,0,0,-1],
+        [1,1,1,-1,-1,-1],
+        [1,1,2,-1,1,-3]
     ];
 c.height = c.width = h;
 c = c.getContext('2d');
 c.strokeStyle = "#CCC";
 c.lineWidth = 0.5;
+Math.r=Math.random;
 
 function dx(x, y) {
-    var v1 = x/h*6,
-        v2 = y/h*6;
+    with (Math) {
+        var v1 = min(5, max(0, round(x/h*6))),
+            v2 = min(5, max(0, round(y/h*6)));
+    }
 
     // need to do some interpolation
-    return vf[Math.round(v2)][Math.round(v1)];
+    return vf[v2][v1]/10;
 }
 
-function flake(s, x, y, r) {
-    var v = 0;
+function flake(x, y, nth, r) {
+    var v = 0, dc=rnd(100)+400,s=Math.r()*3+2;
     function step() {
         y = 1 + y;
         x = v + x;
-        v = dx(x, y);
+        v += dx(x, y);
         c.translate(x, y);
     }
     this.d = function () {
-        var ii = 9, qs = s / 7, qs2 = 2 * qs;
+        if (!dc--){f.splice(nth, 1);}
+        var ii = 9, qs = s/6, qs2 = 4 * qs, qs3=3*qs;
         c.save();
         step();
+        c.rotate(p*Math.r()*0.25);
         c.beginPath();
         while (ii--) {
             c.lineTo(0, s);
             c.moveTo(0, s - qs2);
-            c.lineTo(qs2, s - qs);
+            c.lineTo(qs3, s - qs);
             c.moveTo(0, s - qs2);
-            c.lineTo(-qs2, s - qs);
+            c.lineTo(-qs3, s - qs);
             c.moveTo(0, 0);
-            c.rotate(Math.PI * 2 * 45 / 360);
+            c.rotate(p/4);
         }
         c.stroke();
         c.closePath();
         c.restore();
     };
+    f.push(this);
 }
 
-var ii = n;
-
-while (ii--) {
-    f.push(new flake(10, 100, 100));
+function rnd(n) {
+    return Math.round(Math.r() * n);
 }
 
 di = setInterval(function () {
+    var ii=f.length;
+    if (!rnd(3)){new flake(Math.r()*h,0,ii++);}
     c.clearRect(0, 0, h, h);
-    ii = n;
     while(ii--){f[ii].d();}
     t++;
-}, 100);
+},50);
