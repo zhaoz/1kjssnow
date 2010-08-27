398e6c6a9c5f1fa3dc96d7374a4d6183a9afc57b
Ziling Zhao <zilingzhao@gmail.com>
2010-08-27 00:00:33 -0700

    some more compression

--- 1k.js
+++ 1k.js
@@ -1,4 +1,4 @@
-c = document.body, f=[],h=600,m=Math,p=m.PI,t=0;
+b = document.body, f=[],h=600,m=Math,p=m.PI,t=0,r=m.random,q=m.round;
     a = [
     //y accel
         1,1,1,1,1,1,
@@ -16,20 +16,18 @@ c = document.body, f=[],h=600,m=Math,p=m.PI,t=0;
         1,1,1,-1,-1,-1,
         1,1,0,-1,1,-1
     ];
-c.style.background = "black";
-c=c.children[0];
+b.style.background = "black";
+c=b.children[0];
 c.height = c.width = h;
 c = c.getContext('2d');
 c.strokeStyle = "#DDD";
 c.lineWidth = 0.3;
-m.r=m.random;
-m.q=m.round;
 
 function k(x, y) {
-    var vx = m.r()/4 * (m.q(m.r()) ? -1 : 1),
-        vy = m.r()+0.5, n,
-        dc=m.q(m.r()*150)+450,
-        s = m.r()*3+2,
+    var vx = r()/4 * (q(r()) ? -1 : 1),
+        vy = r()+0.5, n,
+        dc=q(r()*150)+450,
+        s = r()*3+2,
         qs = s/6, qs2 = 4 * qs, qs3=3*qs;
     this.d = function () {
         if (!dc--) {
@@ -57,7 +55,7 @@ function k(x, y) {
             // end step
 
             // random rotation
-            rotate(p*m.r()*0.25);
+            rotate(p*r()*0.25);
             beginPath();
             while (n--) {
                 lineTo(0, s);
@@ -77,8 +75,8 @@ function k(x, y) {
 }
 
 setInterval(function () {
-    if (!m.q(m.r()*2)) {
-        new k(m.r()*h, 0);
+    if (!q(r()*2)) {
+        new k(r()*h, 0);
     }
     ii=f.length;
     c.clearRect(0, 0, h, h);
